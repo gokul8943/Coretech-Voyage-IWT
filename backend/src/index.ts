966950028dc7ab30b7e354ef5654ff3e0784f5d4
Gokul M R <gokulmr331@gmail.com>
2025-11-12 09:13:57 +0530

    update:server error fixing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,12 @@
-import  express  from 'express'
+import express from 'express'
 import dotenv from 'dotenv'
+
 
 dotenv.config()
 const app = express()
-const port = process.env.PORT
+const port = process.env.port
 
 
-app.listen(port,()=>{
-    console.log("server is running");
-    
+app.listen(port,() =>{
+    console.log("server is running")
 })
-
-
