9f6883ccde2dc0139ecc63f1c2921416d4316d82
Gokul M R <gokulmr331@gmail.com>
2025-11-14 09:00:09 +0530

    feat:routes for auth created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import authRoutes from '../src/routes/authRoute'
 
 
 dotenv.config()
 const app = express()
 const port = process.env.port
 
+app.use('/api/auth', authRoutes)
 
-app.listen(port,() =>{
+app.listen(port, () => {
     console.log("server is running")
 })
---
+++ backend/src/routes/authRoute.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { login, signUp } from "../controllers/AuthController";
+
+
+const router = Router()
+
+router.post('/signup', signUp)
+
+router.post('/login', login)
+
+export default router
